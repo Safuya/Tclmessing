84351459985801bc37b4dd8c426dbbfbb7b420a0
Safuya <roberts.ginger.email@gmail.com>
2015-03-05 07:49:16 +0000

    Changed to procedure
     Changes to be committed:
    	modified:   random_characters.tcl

--- random_characters.tcl
+++ random_characters.tcl
@@ -2,16 +2,18 @@
 source ./random.tcl
 puts -nonewline "Choose a random string length: "
 flush stdout
-set length [gets stdin]
-set random_string ""
 
-for {set i 1} {$i <= [expr $length]} {incr i} {
-    set random_ascii [random_min_max 65 116]
-    if {$random_ascii > 90} {
-        set random_ascii [expr $random_ascii + 6]
+proc random_string {length} {
+    set random_string ""
+    for {set i 1} {$i <= [expr $length]} {incr i} {
+        set random_ascii [random_min_max 65 116]
+        if {$random_ascii > 90} {
+            set random_ascii [expr $random_ascii + 6]
+        }
+        set character [format %c $random_ascii]
+        set random_string "$random_string$character"
     }
-    set character [format %c $random_ascii]
-    set random_string "$random_string$character"
+    return $random_string
 }
 
-puts $random_string
+puts [random_string [gets stdin]]
